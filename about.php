bd381f7e98deeaa0169913dd1c92bb40aeeabd61
PrinceNiyonshuti <npprince47@gmail.com>
2021-03-02 19:52:40 +0200

    after about us and photo

--- about.php
+++ about.php
@@ -1,145 +1,103 @@
     <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 text-left">
-            <div class="section-title mb-5">
-              <h2>About Us</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-left">
+                    <div class="section-title mb-5">
+                        <h2>About Us</h2>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>
+                        <img src="images/Owner.jpg" alt="Image" class="img-fluid">
+                    </p>
+                </div>
+                <div class="col-lg-6 pl-md-5">
+                    <p>
+                        Hello I’m Nkurunziza Emmanuel, welcome to my personal finance and entrepreneur blog! I created
+                        this
+                        blog as a place to empower others to have Financial and Entrepreneurial skills.
+                        I am a young adult who graduated from the former School of Finance and Banking (SFB), now
+                        University
+                        of Rwanda College of Business and Economics with undergraduate business BBA Finance degree and
+                        did
+                        courses in Enterprise management and money management skills. Igishoro was created to empower
+                        people especially the youth by giving them entrepreneurial Skills and financial literacy skills
+                        that will help
+                        have basic money management skills.
+                        Research shows that by 2030 More than 300 Million Africans mostly the Youth will be unemployed
+                        and
+                        in need of jobs. Many people have low or no entrepreneurial and financial literacy skills and
+                        this hinders
+                        them to take their Ideas and financial decisions to a further step and create wealth out them.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="row">
-          <div class="col-lg-6">
-            <p>
-              <img src="images/big_img_1.jpg" alt="Image" class="img-fluid">
-            </p>
-          </div>
-          <div class="col-lg-6 pl-md-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam qui obcaecati eaque odio, alias nulla quae, aliquid, rerum consequuntur, quaerat blanditiis cupiditate eos doloribus dolor debitis! Ex eius deleniti, soluta!</p>
-            <p>Facilis sit molestiae deserunt quo corporis culpa dolorum animi architecto illum sapiente. Asperiores, placeat animi distinctio provident adipisci.</p>
-            <ul class="ul-check list-unstyled success mt-5">
-              <li>Lorem ipsum dolor sit.</li>
-              <li>Cupiditate dolores rerum, consequatur!</li>
-              <li>Quia dolor molestias voluptatem?</li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
 
 
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8">
+    <div class="site-section" style="padding: 0%;">
+        <div class="container">
             <div class="row">
-              <div class="col-12">
-                <div class="section-title">
-                  <h2>Editor's Pick</h2>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="post-entry-1">
-                  <a href="post-single.html"><img src="images/img_h_1.jpg" alt="Image" class="img-fluid"></a>
-                  <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                  <div class="post-meta">
-                    <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                    <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="post-entry-2 d-flex bg-light">
-                  <div class="thumbnail" style="background-image: url('images/img_v_1.jpg')"></div>
-                  <div class="contents">
-                    <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                    <div class="post-meta">
-                      <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                      <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="post-entry-1">
+                                <h2>
+                                    <a href="#">
+                                        Starting a business from scratch and keeping it running requires a lot of
+                                        efforts.
+                                    </a>
+                                </h2>
+                                <p>
+                                    Anyone can be an entrepreneur, Starting a small business is an exciting venture, but
+                                    succeeding at
+                                    managing the business is also not an easy task but most importantly managing your
+                                    finances are the key
+                                    moments of your life, all it takes is that one must have passion, ambition, dream
+                                    big, think long term, be
+                                    focused and use talents and skills that one has. The most important assets we people
+                                    have are our mind,
+                                    information and time, and if our mind is well trained and developed they can create
+                                    wealth. We were
+                                    not taught financial literacy at school, so we should change our thinking to become
+                                    financially literate by
+                                    preparing ourselves to learn. We must invest your time in financial education so
+                                    that we build a firm
+                                    financial foundation.
+                                </p>
+                            </div>
+
+                            <div class="post-entry-1">
+                                <h2>
+                                    <a href="#">
+                                        Capital is not the only factor that one needs for a business success, but also
+                                        better execution strategy of a business is required.
+                                    </a>
+                                </h2>
+                                <p>
+                                    While having capital is certainly important, it is not a guarantee to the business
+                                    success. Most funded
+                                    Small and Medium-sized Enterprises (SMES) and Projects do fail not necessarily as a
+                                    result of not being
+                                    funded or not given seed capital, but rather lack of entrepreneurial skills,
+                                    financial literacy skills and
+                                    poor execution. We always focus on capital as the most factor to the success of a
+                                    business, but capital is
+                                    just a catalyst and not a replacement for better execution strategy. As capital is
+                                    not always enough, we
+                                    should also learn to start small and grow big and do more with less that we have
+                                    .This is a great principle
+                                    of value to learn as in most cases capital is always not enough to start a business.
+                                </p>
+                            </div>
+
+                        </div>
                     </div>
-                  </div>
                 </div>
-
-                <div class="post-entry-2 d-flex">
-                  <div class="thumbnail" style="background-image: url('images/img_v_2.jpg')"></div>
-                  <div class="contents">
-                    <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                    <div class="post-meta">
-                      <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                      <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="post-entry-2 d-flex">
-                  <div class="thumbnail" style="background-image: url('images/img_v_3.jpg')"></div>
-                  <div class="contents">
-                    <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                    <div class="post-meta">
-                      <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                      <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="section-title">
-              <h2>Trending</h2>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">01</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">02</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">03</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">04</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-            
-            <p>
-              <a href="#" class="more">See All Trends <span class="icon-keyboard_arrow_right"></span></a>
-            </p>
-
-          </div>
         </div>
-      </div>
     </div>
